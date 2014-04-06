9fbf4cf300f0801f16f03c496f77225d0933cb5a
Amit D. Bansil <amit@bansil.org>
2014-04-06 14:40:21 -0400

    .

--- robocompviz/process.py
+++ robocompviz/process.py
@@ -1,5 +1,6 @@
 import csv
 
+#utility functions
 def readCSV(fileName):
 	with(open(fileName, 'U')) as file_:
 		data = csv.reader(file_)
@@ -12,6 +13,14 @@ def readCSV(fileName):
 				retRow[header] = cell
 			ret.append(retRow)
 		return ret
+
+def incrementDict(dict_, key, amount=1):
+	if not key in dict_:
+		dict_[key] = 0
+
+	dict_[key] += amount
+
+#data structures
 
 #division
 #round
@@ -40,22 +49,26 @@ competitions = readCSV('competitions.csv')
 #sp
 rankings = readCSV('rankings.csv')
 
+#accumulation variables
 maxRedScoreEver = 0
 maxBlueScoreEver = 0
 redWins = 0
 blueWins = 0
 winsByRobot = {}
 pointsByRobot = {}
-def incrementDict(dict_, key, amount=1):
-	if not key in dict_:
-		dict_[key] = 0
-
-	dict_[key] += amount
+#TODO min score ever for red & blue
+#TODO min score ever regardless of color
+#TODO max score ever regardless of color
+#TODO average score for red, blue, and & regardless of color
+#wins by team number sorted by team
+#losses by robot name
+#losses by team number sorted by team
+#start looking at http://research.microsoft.com/en-us/projects/trueskill/details.aspx
 
 for competition in competitions:
 	maxRedScoreEver = max(competition['redscore'], maxRedScoreEver)
 	maxBlueScoreEver = max(competition['bluescore'], maxBlueScoreEver)
-	
+
 	if competition['winner'] == 'Red':
 		redWins += 1
 		winner1 = competition['red1']
@@ -68,12 +81,16 @@ for competition in competitions:
 	incrementDict(winsByRobot, winner1)
 	incrementDict(winsByRobot, winner2)
 
-print redWins
-print blueWins
+
+#print computations
+print 'redWins:', redWins
+print 'blueWins', blueWins
+print 'maxRedScoreEver:', maxRedScoreEver
+print 'maxBlueScoreEver:', maxBlueScoreEver
 
 winsByRobotSorted = winsByRobot.items()
 winsByRobotSorted.sort(key=lambda x:x[1], reverse=True)
+print ''
+print 'ROBOT NAME, WINS'
+print '----------'
 print '\n'.join([str(item) for item in winsByRobotSorted])
-
-print maxRedScoreEver
-print maxBlueScoreEver
