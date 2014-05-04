6f12df22fb19a7bf1d4d09a054ec7a8a54d5ca06
cdelong <cabell.delong@gmail.com>
2014-05-04 11:15:37 -0400

    More calculations
    Added calculations for average score for red, blue, and regardless of
    color (doesn’t yet work), minimum and maximum scores ever for both
    colors and overall, and a list of losses in descending order (similar
    to the list of wins already present).

--- robocompviz/process.py
+++ robocompviz/process.py
@@ -52,41 +52,74 @@ rankings = readCSV('rankings.csv')
 #accumulation variables
 maxRedScoreEver = 0
 maxBlueScoreEver = 0
+minRedScoreEver = 0
+minBlueScoreEver = 0
+maxScoreEver = 0
+minScoreEver = 0
+redPoints = 0
+bluePoints = 0
+allPoints = 0
+redAvg =0
+blueAvg = 0
+allAvg = 0
 redWins = 0
 blueWins = 0
 winsByRobot = {}
+lossesByRobot = {}
 pointsByRobot = {}
-#TODO min score ever for red & blue
-#TODO min score ever regardless of color
-#TODO max score ever regardless of color
-#TODO average score for red, blue, and & regardless of color
-#wins by team number sorted by team
-#losses by robot name
-#losses by team number sorted by team
-#start looking at http://research.microsoft.com/en-us/projects/trueskill/details.aspx
 
 for competition in competitions:
 	maxRedScoreEver = max(competition['redscore'], maxRedScoreEver)
 	maxBlueScoreEver = max(competition['bluescore'], maxBlueScoreEver)
 
+	minRedScoreEver = min(competition['redscore'], minRedScoreEver)
+	minBlueScoreEver = min(competition['bluescore'], minBlueScoreEver)
+
+	maxScoreEver = max(competition['redscore'], competition['bluescore'], maxScoreEver)
+	minScoreEver = min(competition['redscore'], competition['bluescore'], minScoreEver)
+
+	"""
+	redPoints += competition['redscore']
+	bluePoints += competition['bluescore']
+	allPoints = redPoints + bluePoints
+	"""
+
 	if competition['winner'] == 'Red':
 		redWins += 1
 		winner1 = competition['red1']
-		winner2 = competition['red2'] 
+		winner2 = competition['red2']
+		loser1 = competition['blue1']
+		loser2 = competition['blue2']
 	else:
 		blueWins += 1
 		winner1 = competition['blue1']
 		winner2 = competition['blue2']
+		loser1 = competition['red1']
+		loser2 = competition['red2']
 
 	incrementDict(winsByRobot, winner1)
 	incrementDict(winsByRobot, winner2)
+	incrementDict(lossesByRobot, loser1)
+	incrementDict(lossesByRobot, loser2)
 
+"""
+redAvg = redPoints/len(competitions)
+blueAvg = bluePoints/len(competitions)
+allAvg = allPoints/len(competitions)
+"""
 
 #print computations
 print 'redWins:', redWins
 print 'blueWins', blueWins
 print 'maxRedScoreEver:', maxRedScoreEver
 print 'maxBlueScoreEver:', maxBlueScoreEver
+print 'minRedScoreEver', minRedScoreEver
+print 'minBlueScoreEver', minBlueScoreEver
+print 'maxScoreEver', maxScoreEver
+print 'minScoreEver', minScoreEver
+print 'redAvg', redAvg
+print 'blueAvg', blueAvg
+print 'allAvg', allAvg
 
 winsByRobotSorted = winsByRobot.items()
 winsByRobotSorted.sort(key=lambda x:x[1], reverse=True)
@@ -94,3 +127,10 @@ print ''
 print 'ROBOT NAME, WINS'
 print '----------'
 print '\n'.join([str(item) for item in winsByRobotSorted])
+
+lossesByRobotSorted = lossesByRobot.items()
+lossesByRobotSorted.sort(key=lambda x:x[1], reverse=True)
+print ''
+print 'ROBOT NAME, LOSSES'
+print '----------'
+print '\n'.join([str(item) for item in lossesByRobotSorted])
